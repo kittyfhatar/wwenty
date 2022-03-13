ef0d62dae02cdad16a4746f53b03dcc588c70713
refrain <64312760+wwwenty@users.noreply.github.com>
2022-03-13 15:09:35 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+const preloader = document.querySelector(".preloader");
+const button = document.querySelector(".preloader-content_button");
+let animInterval;
+let loadInterval;
+
+const videoId = "4lJ_rC0dvy8"
+YT.ready(_=>{
+    window.player = new YT.Player("player", {
+        videoId: videoId,
+        playerVars: { "autoplay": 0, "controls": 0, "loop": 1, "playlist": videoId},
+        events: {
+            "onReady": e => {
+                //Autoplay disabled so fuck this
+                //player.pauseVideo();
+                //player.mute();
+                e.target.setVolume(0);
+                clearInterval(loadInterval)
+               
+                });
+            }
+        }
+    });
+})
+
+const xd = ["⠟","⠯","⠷","⠾","⠽","⠻"].reverse();
+let i = 0;
+loadInterval = setInterval( _ => {
+    if (i==xd.length) i=0
+    button.innerText = xd[i];
+    i++
+},100)
+
+let j = 1;
+function preloaderAnim() {
+    j -= .01;
+    preloader.style.opacity = j;
+
+    if (j <= 0) {
+        clearInterval(animInterval);
+        setTimeout(() => {preloader.removeAttribute("style");}, 50);
+        setTimeout(() => {preloader.style.display = "none";}, 60);
+    };
+
+    if (j <= 1) {
+        player.unMute();
+        player.playVideo();
+    };
+};
